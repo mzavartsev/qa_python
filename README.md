1999ad741a832013afdd18d5a0c27a43626e634d
Maxim <zavartsevm@gmail.com>
2024-09-18 23:38:51 +0300

    first_commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # qa_python
+
+test_set_book_genre_added_one_book_with_genre_get_one_book_with_genre
+Добавляю одну книгу с жанром, проверяю,что получаю одну книгу с жанром
+
+test_get_book_genre_zero_books_empty_list
+Ничего не добавляю, проверяю, что список пустой
+
+test_get_book_genre_one_book_with_genre_get_genre_of_book
+Добавляю одну книгу с жанром, проверяю жанр книги
+
+test_get_books_with_specific_genre_zero_books_empty_list
+Ничего не добавляю, проверяю, что список пустой
+
+test_get_books_genre_added_one_book_book_received
+Добавляю одну книгу, проверяю словарь
+
+test_get_books_for_children_added_one_book_with_genre_get_one_book_with_genre_for_children
+Добавляю одну книгу с жанром, проверяю, что книга не попала в список, тк не подошла по жанру
+
+test_delete_book_from_favorites_one_book_get_empty_list
+Добавляю одну книгу с жанром,удаляю, проверяю, что список пустой
+
+test_get_list_of_favorites_books_empty_list_get_empty_list
+Ничего не добавляю, проверяю,что возвращается пустой список
+
--- tests.py
+++ tests.py
@@ -8,9 +8,8 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    def test_add_new_book_add_two_books(self, collector):
         # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
 
         # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
@@ -18,7 +17,47 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_set_book_genre_added_one_book_with_genre_get_one_book_with_genre(self, collector):
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_genre('Гарри Поттер','Ужасы')
+        assert collector.get_book_genre('Гарри Поттер') == 'Ужасы'
+
+    def test_get_book_genre_zero_books_empty_list(self, collector):
+        assert collector.get_book_genre('Гордость и предубеждение и зомби') is None
+
+    def test_get_book_genre_one_book_with_genre_get_genre_of_book(self, collector):
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_genre('Гарри Поттер','Ужасы')
+        assert collector.get_book_genre('Гарри Поттер') is 'Ужасы'
+
+    def test_get_books_with_specific_genre_zero_books_empty_list(self, collector):
+        assert collector.get_books_with_specific_genre('Гордость и предубеждение и зомби') == []
+
+    def test_get_books_genre_added_one_book_book_received(self, collector):
+        collector.add_new_book('Гарри Поттер')
+        assert collector.get_books_genre() == {'Гарри Поттер':''}
+
+    def test_get_books_for_children_added_one_book_with_genre_get_one_book_with_genre_for_children(self, collector):
+        collector.add_new_book('Гарри Поттер')
+        collector.set_book_genre('Гарри Поттер','Ужасы')
+        assert collector.get_books_for_children() == []
+
+    def test_add_book_in_favorites_added_one_book_with_genre_get_one_book_in_favorites(self, collector):
+        collector.add_new_book('Гарри Поттер')
+        collector.add_book_in_favorites('Гарри Поттер')
+        assert 'Гарри Поттер' in collector.get_list_of_favorites_books()
+
+
+    def test_delete_book_from_favorites_one_book_get_empty_list(self, collector):
+        collector.add_new_book('Гарри Поттер')
+        collector.add_book_in_favorites('Гарри Поттер')
+        collector.delete_book_from_favorites('Гарри Поттер')
+        assert collector.get_list_of_favorites_books() == []
+
+    def test_get_list_of_favorites_books_empty_list_get_empty_list(self, collector):
+        assert collector.get_list_of_favorites_books() == []
